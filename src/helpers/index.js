a69feaa63bab550dff85bba2dfe5a2c315a87ca3
codaelux <bizmindx@gmail.com>
2020-04-16 11:42:59 +0100

    helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,1 +1,25 @@
+import {
+	digModel,
+	generateRandomUsername,
+	generateAvatar,
+	generateUser
+} from './misc';
 
+
+import { comparePassword, hashPassword } from './password';
+import {
+	registerSchema,
+	resetPasswordSchema
+} from './validators';
+
+
+export {
+	digModel,
+	generateRandomUsername,
+	generateAvatar,
+	generateUser,
+	hashPassword,
+	registerSchema,
+	resetPasswordSchema,
+	comparePassword
+};
