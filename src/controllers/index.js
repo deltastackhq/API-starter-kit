44e1588285c1d8926809be1dcafb407e6a2a59ca
codaelux <bizmindx@gmail.com>
2020-04-16 11:42:05 +0100

    controller entry

---
+++ src/controllers/index.js
@@ -0,0 +1,24 @@
+import {
+	signup,
+	resendVerification,
+	verifyToken,
+	googleRedirect,
+	googleAuth,
+	refreshRoute,
+	login,
+	logout
+} from './authController';
+import { requestPasswordReset, resetPassword } from './misc';
+
+export {
+	requestPasswordReset,
+	resetPassword,
+	signup,
+	resendVerification,
+	verifyToken,
+	googleRedirect,
+	googleAuth,
+	refreshRoute,
+	login,
+	logout
+};
